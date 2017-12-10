5f777f4f65113a3aa201f7de97afb46c1452bd7c
遼太郎 金 <150568@jc-21.jp>
2017-12-10 23:41:25 +0900

    テスト

--- SamplePage/SamplePage/BookPage.xaml.cs
+++ SamplePage/SamplePage/BookPage.xaml.cs
@@ -18,7 +18,8 @@ namespace SamplePage
 
             this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
 
-            this.listView.ItemsSource = Enumerable.Range(0, 100).Select(n => $"aaa");
+            //this.listView.ItemsSource = Enumerable.Range(0, 100).Select(n => $"aaa");
+            this.listView.ItemsSource = Enumerable.Range(0, 100).Select(n => user.Name);
         }
     }
 }
