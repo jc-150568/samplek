68da494dfd5fa14d6e417264d2d408874a366173
遼太郎 金 <150568@jc-21.jp>
2017-12-12 19:19:33 +0900

    実験じゃい

--- SamplePage/SamplePage/BookPage.xaml.cs
+++ SamplePage/SamplePage/BookPage.xaml.cs
@@ -12,20 +12,34 @@ namespace SamplePage
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BookPage : ContentPage
     {
+        private int i = 1;
         public BookPage()
         {
             InitializeComponent();
 
-            /*var query = UserModel.selectUser(); //中身はSELECT * FROM [User] limit 15
-            var query2 = UserModel.countUser();
+            var query = UserModel.selectUser(); //中身はSELECT * FROM [User] limit 15
+            var query2 = UserModel.countUser(i);
+
+            var s = new List<String>();
+
+            foreach (var user in query2)
+            {
+                for (i = 1; i == query.Count; i++)
+                {
+                    s[i] = user.Name;
+                    s.Add(s[i]);
+
+                }
+            }
+
 
             foreach (var user in query)
             {
                 this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
-                
-                this.listView.ItemsSource = Enumerable.Range(0,query.Count).Select(n => user.Name);
 
-            }*/
+                this.listView.ItemsSource = Enumerable.Range(0, query.Count).Select(n => s);
+
+            }
         }
 
             
--- SamplePage/SamplePage/UserModel.cs
+++ SamplePage/SamplePage/UserModel.cs
@@ -266,13 +266,13 @@ namespace SamplePage
             }
         }
 
-        public static List<UserModel> countUser()
+        public static List<UserModel> countUser(int i)
         {
             using (SQLiteConnection db = new SQLiteConnection(App.dbPath))
             {
                 try
                 {
-                    return db.Query<UserModel>("SELECT COUNT(*) FROM[Book] limit 15");
+                    return db.Query<UserModel>("SELECT Name FROM[Book] where [No] =" + i + " limit 15");
                 }catch(Exception e)
                 {
                     System.Diagnostics.Debug.WriteLine(e);
