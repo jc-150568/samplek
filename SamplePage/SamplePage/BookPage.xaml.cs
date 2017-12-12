56c1825b3ef8f6a4a1697b608769e7dc7353aa8a
遼太郎 金 <150568@jc-21.jp>
2017-12-13 02:54:41 +0900

    select

--- SamplePage/SamplePage/BookPage.xaml.cs
+++ SamplePage/SamplePage/BookPage.xaml.cs
@@ -12,34 +12,44 @@ namespace SamplePage
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BookPage : ContentPage
     {
-        private int i = 1;
+       // private int i = 1;
         public BookPage()
         {
             InitializeComponent();
 
             var query = UserModel.selectUser(); //中身はSELECT * FROM [User] limit 15
-            var query2 = UserModel.countUser(i);
-
-            var s = new List<String>();
-
-           /* foreach (var user in query2)
-            {
-                for (i = 1; i == query.Count; i++)
-                {
-                    s[i] = user.Name;
-                    s.Add(s[i]);
-
-                }
-            }*/
-
-
+            var layout = new StackLayout { HorizontalOptions = LayoutOptions.Center };
             foreach (var user in query)
             {
-                this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
-
-                this.listView.ItemsSource = Enumerable.Range(0, query.Count).Select(n => user.Name);
-
+                //Userテーブルの名前列をLabelに書き出します
+                layout.Children.Add(new Label { Text = user.Id.ToString() });
+                layout.Children.Add(new Label { Text = user.Name });
             }
+            Content = layout;
+            // var query2 = UserModel.countUser(i);
+
+            // var s = new List<String>();
+
+            /* foreach (var user in query2)
+             {
+                 for (i = 1; i == query.Count; i++)
+                 {
+                     s[i] = user.Name;
+                     s.Add(s[i]);
+
+                 }
+             }
+
+
+             foreach (var user in query)
+             {
+                 this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+
+                 this.listView.ItemsSource = Enumerable.Range(0, query.Count).Select(n => user.Name);
+
+             }*/
+
+
         }
 
             
