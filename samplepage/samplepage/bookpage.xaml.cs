d50d06ae9802ed8e126d1e05d2fd51edbff0b355
遼太郎 金 <150568@jc-21.jp>
2017-12-13 02:38:28 +0900

    色々コメント

---
+++ samplepage/samplepage/bookpage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SamplePage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BookPage : ContentPage
+    {
+       // private int i = 1;
+        public BookPage()
+        {
+            InitializeComponent();
+
+            var query = UserModel.selectUser(); //中身はSELECT * FROM [User] limit 15
+           // var query2 = UserModel.countUser(i);
+
+           // var s = new List<String>();
+
+           /* foreach (var user in query2)
+            {
+                for (i = 1; i == query.Count; i++)
+                {
+                    s[i] = user.Name;
+                    s.Add(s[i]);
+
+                }
+            }*/
+
+
+            foreach (var user in query)
+            {
+                this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+
+                this.listView.ItemsSource = Enumerable.Range(0, query.Count).Select(n => user.Name);
+
+            }
+        }
+
+            
+    }
+}
